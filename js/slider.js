db4c5140749a551c86c9c48ee7258f6bd2436d09
gtashnik <phpshnov@yandex.ru>
2018-08-07 14:20:33 +0300

    adding js script of slider

---
+++ js/slider.js
@@ -0,0 +1,44 @@
+/* 
+Simple background slider by Dmitry 
+https://findimension.com 
+
+
+
+*/
+
+
+function showSlide() {
+        
+document.getElementById('slider').style.backgroundImage = "url(pictures/1.jpg)";
+        document.getElementById('item1').style.color = "blue";
+       document.getElementById('item2').style.color = "white";
+           document.getElementById('item3').style.color = "white";
+           document.getElementById('item4').style.color = "white";
+    }
+    
+    function showSlide2() {
+        
+document.getElementById('slider').style.backgroundImage = "url(pictures/2.jpg)";
+        document.getElementById('item2').style.color = "blue";
+       document.getElementById('item1').style.color = "white";
+           document.getElementById('item3').style.color = "white";
+           document.getElementById('item4').style.color = "white";
+    }
+    
+    function showSlide3() {
+        
+document.getElementById('slider').style.backgroundImage = "url(pictures/3.jpg)";
+        document.getElementById('item3').style.color = "blue";
+       document.getElementById('item1').style.color = "white";
+           document.getElementById('item2').style.color = "white";
+           document.getElementById('item4').style.color = "white";
+    }
+    
+    function showSlide4() {
+        
+document.getElementById('slider').style.backgroundImage = "url(pictures/4.jpg)";
+        document.getElementById('item4').style.color = "blue";
+          document.getElementById('item1').style.color = "white";
+           document.getElementById('item2').style.color = "white";
+           document.getElementById('item3').style.color = "white";
+    }
